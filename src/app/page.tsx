90758fe42fbca6501cf861b6d922bf33e4909f0b
ApuKM <apukumar180@gmail.com>
2026-08-21 12:07:29 +0600

    Add: Navbar component with shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Navbar from "@/components/Navbar";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-
       <main>
         <h1>ResuMate AI</h1>
       </main>
